53eff5733c81bda0d3a81fb893dd54b56dcf10ca
jatin7425 <jatinvishwakarma4310@gmail.com>
2026-08-16 19:04:31 +0530

    asdfqwerty

--- api/anthropic_adaptions.js
+++ api/anthropic_adaptions.js
@@ -468,6 +468,7 @@ export default async function handler(req, res) {
 
         try {
           const resp = await forwardRequest(item, upstreamBase, req);
+          console.log(`Upstream response status: ${resp}`);
 
           if (resp.status >= 200 && resp.status < 500) {
             const rawText = typeof resp.data === 'string' ? resp.data : resp.data?.toString?.() || '';
